400335ee62b525abf93c4871da5f4860a363e4b0
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-20 15:28:31 +0100

    Add: exercise 2 initial code

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Usate poi la funzione per decretare se la parola inserita dall'utente è palindr
 
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
-Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione). Dichiariamo chi ha vinto.
+Sommiamo i due numeri e stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione). Dichiariamo chi ha vinto.
 
 ## Consigli
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* PALINDROME */
+
 function isPalindrome(word) {
     for (let i = 0; i < word.length; i++) {
         let reverseIndex = word.length - i - 1;
@@ -21,3 +23,36 @@ if (isPalindrome(word)) {
 } else {
     console.log("The word " + word + " IS NOT palindrome.")
 }
+
+/* EVEN OR ODD */
+
+function getUserChoise() {
+    let userChoise = '';
+    while (userChoise != '0' && userChoise != '1') {
+        userChoise = prompt("Type 0 to choose EVEN or 1 to choose ODD: ");
+    }
+    return userChoise;
+}
+
+function getUserNumber() {
+    let userNumber = 0;
+    while (userNumber < 1 || userNumber > 5) {
+        userNumber = prompt("Now enter a number between 1 and 5 (included): ");
+    }
+    return userNumber;
+}
+
+function getRandomNumber(upperLimit, lowerLimit) {
+    return Math.floor(Math.random() * (upperLimit - lowerLimit + 1) + lowerLimit)
+}
+
+let userChoise = getUserChoise();
+let userNumber = getUserNumber();
+let randomNumber = getRandomNumber(0, 6);
+
+
+
+
+
+
+
